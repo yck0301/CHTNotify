45e554d401368a72e0e6a09e700e82105bae51f9
yck0301 <yck0301@cht.com.tw>
2022-12-08 15:36:53 +0800

    add other methods

--- Controllers/NotifyController.cs
+++ Controllers/NotifyController.cs
@@ -30,4 +30,82 @@ public class NotifyController : ControllerBase
         
         return response;
     }
+
+    /// <summary>
+    /// 發送: Teams
+    /// </summary>
+    /// <returns></returns>
+    [HttpPost("teams")]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public void PostTeamsNotify()
+    {
+    }
+
+    /// <summary>
+    /// 發送: 簡訊
+    /// </summary>
+    /// <returns></returns>
+    [HttpPost("sms")]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public void PostSMSNotify()
+    {
+    }
+
+    /// <summary>
+    /// 發送: Email
+    /// </summary>
+    /// <returns></returns>
+    [HttpPost("email")]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public void PostEmailNotify()
+    {
+    }
+
+    /// <summary>
+    /// 發送: 客製化平台
+    /// </summary>
+    /// <returns></returns>
+    [HttpPost("someplace")]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public void PostSomeplaceNotify()
+    {
+    }
+
+
+    /// <summary>
+    /// 取得發送群組
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("group")] // http://domain/notify/group/g1
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public void GetGroup()
+    {
+    }
+
+    /// <summary>
+    /// 編輯群組人員資料
+    /// </summary>
+    /// <returns></returns>
+    [HttpPatch("group")] // http://domain/notify/group/g1?memberId=memberId1
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public void PatchGroupMember(int memberId)
+    {
+    }
+
+    /// <summary>
+    /// 刪除群組人員資料
+    /// </summary>
+    /// <returns></returns>
+    [HttpDelete("group")] // http://domain/notify/group/g1?memberId=memberId1
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public void DeleteGroupMember(int memberId)
+    {
+    }
 }
--- LineNotify.cs
+++ LineNotify.cs
@@ -7,11 +7,11 @@ public class LineNotify
   /// Line的token
   /// </summary>
   [Required]
-  public string token { get; set; }
+  public string token { get; set; } = string.Empty;
 
   /// <summary>
   /// 你要打的訊息
   /// </summary>
   [Required]
-  public string message { get; set; }
+  public string message { get; set; } = string.Empty;
 }
